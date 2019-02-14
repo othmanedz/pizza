19e337b211ee36d611a005982c30419d55da8989
Manal <manal.bekao@gmail.com>
2019-02-14 17:41:21 +0100

    command

--- src/main/java/userManager/service/Command.java
+++ src/main/java/userManager/service/Command.java
@@ -6,8 +6,52 @@ public class Command {
 	static int random=0;
 	
 
-	int id;
+	int idCommand,idUser;
 	List<Pizza> commandedPizzas;
-	boolean state;
+	boolean isPaid;
+
+	
+	public Command() {}
+	
+	public Command(int idUser, List<Pizza> pizzas) {
+		
+		this.idUser=idUser;
+		this.commandedPizzas=pizzas;
+		
+		this.idCommand=random++;
+	}
+	
+	public int getIdCommand() {
+		return idCommand;
+	}
+
+	public void setIdCommand(int idCommand) {
+		this.idCommand = idCommand;
+	}
+
+	public int getIdUser() {
+		return idUser;
+	}
+
+	public void setIdUser(int idUser) {
+		this.idUser = idUser;
+	}
+
+	public List<Pizza> getCommandedPizzas() {
+		return commandedPizzas;
+	}
+
+	public void setCommandedPizzas(List<Pizza> commandedPizzas) {
+		this.commandedPizzas = commandedPizzas;
+	}
+
+	public boolean isPaid() {
+		return isPaid;
+	}
+
+	public void setPaid(boolean isPaid) {
+		this.isPaid = isPaid;
+	}
+
 	
 }
